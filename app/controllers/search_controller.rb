8f68d5d7f2c73727978897150f57037e166f299a
MasonHolland <masontholland@gmail.com>
2017-07-26 10:53:04 -0600

    test passing, no time for refactor. moving onto api creation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
-    binding.pry
     conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores(area(#{params[:search]},25))?format=json&show=storeType,longName,distance,phone&apiKey=#{ENV["best_buy_key"]}")
-    response = conn.get '/search'
+    response = conn.get
     parsed = JSON.parse(response.body)
+    @stores = parsed["stores"]
+    flash[:success] = "#{parsed["total"]} Total Stores"
   end
 end
